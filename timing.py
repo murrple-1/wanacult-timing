30624fff4ffc00a2c058e7be4cac1181f9a48dca
Murray Christopherson <murraychristopherson@gmail.com>
2022-11-05 21:05:55 -0700

    first pass at the calculation

--- timing.py
+++ timing.py
@@ -5,46 +5,63 @@ import datetime
 from pytimeparse.timeparse import timeparse
 
 
-def main() -> None:
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("chorus_splits")
     parser.add_argument("speed_up", type=float)
-    parser.add_argument("-s", "--start-slow", action="store_true")
-    parser.add_argument("-c", "--start-chorus", action="store_true")
 
     args = parser.parse_args()
 
-    is_slow = args.start_slow
-
-    song_chunk_totals = [float(timeparse(split)) for split in args.chorus_splits.split(",")]
+    song_chunk_totals = [
+        float(timeparse(split)) for split in args.chorus_splits.split(",")
+    ]
 
     assert song_chunk_totals == sorted(song_chunk_totals)
+    assert len(song_chunk_totals) == len(frozenset(song_chunk_totals))
 
     song_chunks = []
 
-    previous_song_chunk = 0.0
+    song_length_cum = 0.0
     for song_chunk_total in song_chunk_totals:
-        song_chunk = song_chunk_total - previous_song_chunk
-        song_chunks.append(song_chunk)
-        previous_song_chunk = song_chunk
+        song_chunk = song_chunk_total - song_length_cum
+        song_chunks.append((song_chunk, song_length_cum))
+        song_length_cum += song_chunk
 
-    at_speed_song_chunks = []
+    song_length = song_chunks[-1][0] + song_chunks[-1][1]
 
-    for song_chunk in song_chunks:
-        if is_slow:
-            at_speed_song_chunks.append((datetime.timedelta(seconds=song_chunk), is_slow))
+    schedule = []
+
+    are_playing_song = False
+
+    for song_chunk, cum_song_chunk in song_chunks:
+        if are_playing_song:
+            schedule.append((datetime.timedelta(seconds=song_length), True))
         else:
-            at_speed_song_chunks.append((datetime.timedelta(seconds=song_chunk * args.speed_up), is_slow))
+            schedule.append(
+                (
+                    datetime.timedelta(
+                        seconds=(
+                            (song_chunk * args.speed_up)
+                            - (cum_song_chunk / args.speed_up)
+                            - (
+                                (song_length - (cum_song_chunk + song_chunk))
+                                / args.speed_up
+                            )
+                        )
+                    ),
+                    False,
+                )
+            )
 
-        is_slow = not is_slow
-    
-    for seconds, is_slow in at_speed_song_chunks:
-        if is_slow:
-            print(f"SLOW: {seconds}")
+        are_playing_song = not are_playing_song
+
+    for time_of_section, are_playing_song in schedule:
+        if are_playing_song:
+            print(f"SONG: {time_of_section}")
         else:
-            print(f"FAST: {seconds}")
+            print(f"FUCK AROUND: {time_of_section}")
 
-    print("\ntotal: {}".format(sum((sc[0] for sc in at_speed_song_chunks), datetime.timedelta())))
+    print("\ntotal: {}".format(sum((s[0] for s in schedule), datetime.timedelta())))
 
 
 if __name__ == "__main__":
